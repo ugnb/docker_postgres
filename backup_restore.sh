e0b93393e8954fdf736bc2b76376aeb4c06b1efd
ugnb <ugn.cws@gmail.com>
2017-01-24 16:44:18 +0200

    Healthcheck flag

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm -f /tmp/restore_finished
+
 pg_restore --clean --no-acl --no-owner -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" /backups/latest.dump || true
 
+touch /tmp/restore_finished
